79948d3c817009fd604d93654f39b121b4b54d03
HP <saurkumar1994@gmail.com>
2024-11-17 21:50:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className=" bg-blue">
+      <header className="App-header ">
+       <div className="bg-pink">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
